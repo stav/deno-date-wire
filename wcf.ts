117df663c8e5c09b1cf905cd795615192c67028c
Steven Almeroth <fedor@primesite.dev>
2022-06-11 13:52:10 -0400

    expose timestamp

--- wcf.ts
+++ wcf.ts
@@ -1,6 +1,15 @@
-export function date (wcfDate: string) { // "/Date(1649946960000)/"
-  const timestamps = wcfDate.match(/\d+/)
+const tsre = new RegExp(/\d+/)
+
+export function timestamp (wcfDate: string) { // "/Date(1649946960000)/"
+  const timestamps = wcfDate.match(tsre)
   if (timestamps) {
-    return new Date(+timestamps[0])
+    return +timestamps[0]
+  }
+}
+
+export function date (wcfDate: string) { // "/Date(1649946960000)/"
+  const ts = timestamp(wcfDate)
+  if (ts) {
+    return new Date(ts)
   }
 }
